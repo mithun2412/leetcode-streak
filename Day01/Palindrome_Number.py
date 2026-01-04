01903dd18095354579a5fb61a8652808925f59a1
mithun <mithunmi2002@gmail.com>
2026-01-04 19:03:17 +0530

    Day 01: Added Palindrome Number

---
+++ Day01/Palindrome_Number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        temp=x
+        rev=0
+        while x > 0:
+            rev =rev *10 + (x %10)
+            x=x//10
+        if rev == temp:
+            return True 
+        else:
+            return False 
