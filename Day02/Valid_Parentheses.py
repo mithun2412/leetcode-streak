f3462c1e25cde2ecbb7772bfd719a1b54346fee0
mithun <mithunmi2002@gmail.com>
2026-01-05 16:06:57 +0530

    day02_Valid_Parentheses

---
+++ Day02/Valid_Parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        for i in range(len(s)):
+            if s[i]=="(":
+                stack.append(")")
+            elif s[i]=="[":
+                stack.append("]")
+            elif s[i]=="{":
+                stack.append("}")
+            elif len(stack)==0 or s[i]!=stack.pop():
+                return False
+        return len(stack)==0
