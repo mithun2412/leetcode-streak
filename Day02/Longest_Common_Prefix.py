55c79c73e02f9c15af9740e4255625c0a69daad6
mithun <mithunmi2002@gmail.com>
2026-01-05 15:40:50 +0530

    code updated

--- Day02/Longest_Common_Prefix.py
+++ Day02/Longest_Common_Prefix.py
@@ -1,19 +1,20 @@
-from typing import List
-
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
+    def longestCommonPrefix(self, strs):
         if not strs:
             return ""
-    
-        prefix = strs[0]
-    
-        for word in strs[1:]:
-            while not word.startswith(prefix):
-                prefix = prefix[:-1]
-                if not prefix:
-                    return ""
-    
-        return prefix
+
+        result = ""
+
+        for i in range(len(strs[0])):        # go character by character
+            current_char = strs[0][i]
+
+            for s in strs:                   # check this char in all strings
+                if i == len(s) or s[i] != current_char:
+                    return result            # stop immediately
+
+            result += current_char           # add char if all matched
+
+        return result
 
 #Time Complexity: O(S) where S is the sum of all characters in all strings.
 #Space Complexity: O(1)
