ddd8a8b23d034e9e6a7ed398405e1aad5ce47fe7
mithun <mithunmi2002@gmail.com>
2026-02-10 16:15:47 +0530

    updated

--- Day01/Two_Sum.py
+++ Day01/Two_Sum.py
@@ -1,10 +1,38 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i] +nums[j] == target:
+        #             return [i,j]
+        freq={}
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i] +nums[j] == target:
-                    return [i,j]
+            rem = target - nums[i]
+            if rem not in freq:
+                freq[nums[i]]=i
+            else:
+                return list((freq[rem],i))
 
-#Time Complexity: O(n^2)
-#Space Complexity: O(1)
+
+
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return list(set(nums1)&set(nums2))
+    
+
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(len(numbers)):
+            i=0
+            j=len(numbers)-1
+            while i < j:
+                if numbers[i] - numbers[j]>target::
+                    rteurn [i,j]
+                elif numbers[i] - numbers[j]>target:
+                    j-=1
+                else:
+                    i+=1
--- Day02/Sqrt(x).py
+++ Day02/Sqrt(x).py
@@ -1,8 +1,10 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        
-        i = 0
-        while i * i <= x:
-            i += 1
-        
-        return i - 1
+        l, r = 0, x
+        while l <= r:
+            m = (l + r) // 2
+            if m * m <= x:
+                l = m + 1
+            else:
+                r = m - 1
+        return r
